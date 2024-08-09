cf5246cd5c568e63c67d792666cd4f1616de65e4
ValeryCharkin <charkin.valery@gmail.com>
2024-08-09 08:34:48 +0300

    change pro1

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,10 +1,10 @@
 
-    public class BonusMilesService {
+public class BonusMilesService {
 
-        public int calculate(int ticketPrice) {
-            return 10_000 / 20;
-        }
+    public int calculate(int ticketPrice) {
+        return 10_000 / 20;
     }
+}
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int ticketPrice = 10_000;
-        int miles = service.calculate (ticketPrice /20) ; // Количество миль
+        int miles = service.calculate(ticketPrice / 20); // Количество миль
 
         System.out.println();
         System.out.println(miles);
